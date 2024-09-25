2e8a04815300c8ab156138126fae96643991d2cf
Anatolii <Steg92sm13@yandex.ru>
2024-09-25 21:26:52 +0800

    нев проект

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.jmc.mazebank.mazebank {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.jmc.mazebank.mazebank to javafx.fxml;
+    exports com.jmc.mazebank.mazebank;
+}
